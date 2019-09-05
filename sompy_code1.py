1bde0d625217f897f612cb26b453c1e53abd8bf2
profcarlos <31132270+profcarlos@users.noreply.github.com>
2019-09-05 11:10:24 -0300

    Create sompy_code1.py

---
+++ sompy_code1.py
@@ -0,0 +1,32 @@
+import matplotlib.pylab as plt
+# matplotlib inline
+# import sompy as sompy
+import pandas as pd
+import numpy as np
+from time import time
+import sompy
+
+### A toy example: two dimensional data, four clusters
+print("Start...")
+dlen = 200
+Data1 = pd.DataFrame(data= 1*np.random.rand(dlen,2))
+Data1.values[:,1] = (Data1.values[:,0][:,np.newaxis] + .42*np.random.rand(dlen,1))[:,0]
+
+
+Data2 = pd.DataFrame(data= 1*np.random.rand(dlen,2)+1)
+Data2.values[:,1] = (-1*Data2.values[:,0][:,np.newaxis] + .62*np.random.rand(dlen,1))[:,0]
+
+Data3 = pd.DataFrame(data= 1*np.random.rand(dlen,2)+2)
+Data3.values[:,1] = (.5*Data3.values[:,0][:,np.newaxis] + 1*np.random.rand(dlen,1))[:,0]
+
+
+Data4 = pd.DataFrame(data= 1*np.random.rand(dlen,2)+3.5)
+Data4.values[:,1] = (-.1*Data4.values[:,0][:,np.newaxis] + .5*np.random.rand(dlen,1))[:,0]
+
+
+Data1 = np.concatenate((Data1,Data2,Data3,Data4))
+
+fig = plt.figure()
+plt.plot(Data1[:,0],Data1[:,1],'ob',alpha=0.2, markersize=4)
+fig.set_size_inches(7,7)
+plt.show()
